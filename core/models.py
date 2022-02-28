94a8580f99460e38c00a866ff6a00157192139e4
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-28 20:56:44 +0530

    fixed discription error

--- core/models.py
+++ core/models.py
@@ -24,10 +24,10 @@ class Profile(models.Model):
 
 class Movie(models.Model):
     title=models.CharField(max_length=225)
-   # discription=models.TextField(blank=True, null=True)
+    descripion:str=models.TextField()
     created=models.DateTimeField(auto_now_add=True)
     uuid=models.UUIDField(default=uuid.uuid4)
-   # types=models.CharField(max_length=10, choices=MOVIE_CHOICES)
+    type=models.CharField(max_length=10, choices=MOVIE_CHOICES)
     videos=models.ManyToManyField('video')
     flyer=models.ImageField(upload_to='flyers')
     age_limit=models.CharField(max_length=10, choices=AGE_CHOICES)
