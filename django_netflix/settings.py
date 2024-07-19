e29e555cc17384d400f5e23e1ab0144081b69e0e
alanchrissantony <alanchrissantony@gmail.com>
2024-07-19 15:22:27 +0530

    middleware

--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'django_netflix.urls'
